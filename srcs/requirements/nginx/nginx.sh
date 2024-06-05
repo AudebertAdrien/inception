8a9e73c01080a35d60b57e673f556e90dbef2284
adrien <adrien.adbrt@tutanota.com>
2024-06-05 19:37:51 +0200

    allo

---
+++ srcs/requirements/nginx/nginx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IMAGE_NAME="nginx_img"
+CONTAINER_NAME="nginx_cnt"
+IMAGE_NAME="nginx_img"
+
+OLD_CONTAINER_ID=$(docker ps -aq --filter "name=$CONTAINER_NAME")
+OLD_IMAGE_ID=$(docker images -q "$IMAGE_NAME")
+
+
+if [ -n "$OLD_CONTAINER_ID" ]; then
+    docker stop "$OLD_CONTAINER_ID" || true
+    docker rm "$OLD_CONTAINER_ID" || true
+fi
+
+if [ -n "$OLD_IMAGE_ID" ]; then
+    docker rmi "$OLD_IMAGE_ID" > /dev/null
+fi
+
+docker build -t "$IMAGE_NAME" .
+
+docker run -it --name "$CONTAINER_NAME" -p 8080:8080 -p 8443:8443 "$IMAGE_NAME"
