bb9601b068dd707bd8d71db551eeb31af138d9ee
Adrien Audebert <aaudeber@student.42perpignan.fr>
2024-06-15 16:25:38 +0200

    update motoko

--- execute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-<Error>
-<Code>AllAccessDisabled</Code>
-<Message>All access to this object has been disabled</Message>
-<RequestId>X7J755YCS25XA9KQ</RequestId>
-<HostId>LU8yQmO/pCDu30S878S3Q/1S1ki78W4tL8ZmIbfPSoG8KxM2BjpLNUSj2ObfMWqKOK0KbthyJc4=</HostId>
-</Error>
